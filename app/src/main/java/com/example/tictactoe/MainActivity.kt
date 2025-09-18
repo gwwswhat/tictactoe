a68aa7e8b034ab01c674cbc380bf2e6beaddd515
gwwswhat <gwwswhat1@gmail.com>
2025-09-18 17:56:21 +0700

    INITIAL COMMIT

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,37 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity: ComponentActivity {
+	override fun onCreate(savedInstanceState: Bundle?){
+		super.onCreate(savedInstanceState)
+		setContent {
+			my_app()
+		}
+	}
+}
+
+@Composable
+fun my_app(){
+	var text by remember { mutableStateOf("Hello World!") }
+	Column(
+		modifier = Modifier
+		  .fillMaxSize()
+			.padding(16.dp),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally
+	){
+		Text(text = text, fontSize = 24.sp)
+		Spacer(modifier = Modifier.height(20.dp))
+		Button(onClick = { text = "You really clicked huh?" } ){
+			Text("Click me!")
+		}
+	}
+}
